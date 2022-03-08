0b32faa3bfd1202c32b38665f7c8a597215a453a
Eslam Sabry <eslam.sabry.a.t@gmail.com>
2022-03-08 11:58:38 +0300

    update comments

--- www/OneTapSMSRetriever.js
+++ www/OneTapSMSRetriever.js
@@ -1,11 +1,11 @@
 var oneTapSMSRetriever = {};
 
-/* Start the SMS Retriever API and listen for incoming SMS + trigger onSMSArrive event when valid SMS arrives */
+/* Start the SMS Retriever API and listen for incoming SMS and callback */
 oneTapSMSRetriever.registerSMSListner = function(successCallback, failureCallback) {
 	cordova.exec( successCallback, failureCallback, 'oneTapSMSRetriever', 'REGISTER_OTP_SMS_LISTNER', [] );
 };
 
-/* Get the SMS Retriever API app's hash string */
+/* Remove listner */
 oneTapSMSRetriever.unregisterSMSListner = function(successCallback, failureCallback) {
 	cordova.exec( successCallback, failureCallback, 'oneTapSMSRetriever', 'UNREGISTER_OTP_SMS_LISTNER', [] );
 };
