2dba96381d92561b5967c8b1bfcfcf29348efbb2
Eslam Sabry <eslam.sabry.a.t@gmail.com>
2022-03-08 12:36:01 +0300

    Fix service name to OneTapSMSRetriever

--- www/OneTapSMSRetriever.js
+++ www/OneTapSMSRetriever.js
@@ -2,12 +2,12 @@ var oneTapSMSRetriever = {};
 
 /* Start the SMS Retriever API and listen for incoming SMS + trigger onSMSArrive event when valid SMS arrives */
 oneTapSMSRetriever.registerSMSListner = function(successCallback, failureCallback) {
-	cordova.exec( successCallback, failureCallback, 'oneTapSMSRetriever', 'REGISTER_OTP_SMS_LISTNER', [] );
+	cordova.exec( successCallback, failureCallback, 'OneTapSMSRetriever', 'REGISTER_OTP_SMS_LISTNER', [] );
 };
 
 /* Get the SMS Retriever API app's hash string */
 oneTapSMSRetriever.unregisterSMSListner = function(successCallback, failureCallback) {
-	cordova.exec( successCallback, failureCallback, 'oneTapSMSRetriever', 'UNREGISTER_OTP_SMS_LISTNER', [] );
+	cordova.exec( successCallback, failureCallback, 'OneTapSMSRetriever', 'UNREGISTER_OTP_SMS_LISTNER', [] );
 };
 
 
