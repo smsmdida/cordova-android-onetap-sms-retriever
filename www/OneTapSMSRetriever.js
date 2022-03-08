5caedfa29e3bceaa798398e9a52a4c68241b688a
salim-anb <salim_ajabbar@yahoo.co.in>
2022-03-08 18:29:33 +0300

    Update OneTapSMSRetriever.js

--- www/OneTapSMSRetriever.js
+++ www/OneTapSMSRetriever.js
@@ -1,11 +1,38 @@
 var exec = require('cordova/exec');
+var PLUGIN_NAME = 'OneTapSMSRetriever';
 
-/* Start the SMS Retriever API and listen for incoming SMS and callback */
-exports.registerSMSListner = function(successCallback, failureCallback) {
+/* Start the SMS Retriever API and listen for incoming SMS and callback 
+registerSMSListner: function(successCallback, failureCallback) {
 	exec(successCallback, failureCallback, 'OneTapSMSRetriever', 'REGISTER_OTP_SMS_LISTNER', [] );
 };
+*/
+registerSMSListner: function(successCallback, errorCallback) {
+        return new Promise(function(resolve, reject) {
+            exec(function(message) {
+                executeCallback(successCallback, message);
+                resolve(message);
+            }, function(message) {
+                executeCallback(errorCallback, message);
+                reject(message);
+            }, PLUGIN_NAME, 'REGISTER_OTP_SMS_LISTNER', []);
+        });
+    },
 
-/* Remove listner */
+/* Remove listner 
 exports.unregisterSMSListner = function(successCallback, failureCallback) {
 	exec(successCallback, failureCallback, 'OneTapSMSRetriever', 'UNREGISTER_OTP_SMS_LISTNER', [] );
 };
+*/
+unregisterSMSListner: function(successCallback, errorCallback) {
+        return new Promise(function(resolve, reject) {
+            exec(function(message) {
+                executeCallback(successCallback, message);
+                resolve(message);
+            }, function(message) {
+                executeCallback(errorCallback, message);
+                reject(message);
+            }, PLUGIN_NAME, 'UNREGISTER_OTP_SMS_LISTNER', []);
+        });
+    },
+
+module.exports = PLUGIN_NAME;	    
