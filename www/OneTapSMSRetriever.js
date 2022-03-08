c6fc00fff82766da4951b7647ad1849a109126c2
Eslam Sabry <eslam.sabry.a.t@gmail.com>
2022-03-08 15:43:32 +0300

    Update OneTapSMSRetriever.js

--- www/OneTapSMSRetriever.js
+++ www/OneTapSMSRetriever.js
@@ -1,23 +1,11 @@
-// Empty constructor
-function OneTapSMSRetriever() {}
+var exec = require('cordova/exec');
 
-// The function that passes work along to native shells
-// Message is a string, duration may be 'long' or 'short'
-OneTapSMSRetriever.prototype.registerSMSListner = function(successCallback, failureCallback) {
-	cordova.exec( successCallback, failureCallback, 'OneTapSMSRetriever', 'REGISTER_OTP_SMS_LISTNER', [] );
-}
-
-OneTapSMSRetriever.prototype.unregisterSMSListner = function(successCallback, failureCallback) {
-	cordova.exec( successCallback, failureCallback, 'OneTapSMSRetriever', 'UNREGISTER_OTP_SMS_LISTNER', [] );
-}
-
-
-// Installation constructor that binds ToastyPlugin to window
-OneTapSMSRetriever.install = function() {
-  if (!window.plugins) {
-    window.plugins = {};
-  }
-  window.plugins.OneTapSMSRetriever = new OneTapSMSRetriever();
-  return window.plugins.OneTapSMSRetriever;
+/* Start the SMS Retriever API and listen for incoming SMS and callback */
+exports.registerSMSListner = function(successCallback, failureCallback) {
+	exec(successCallback, failureCallback, 'OneTapSMSRetriever', 'REGISTER_OTP_SMS_LISTNER', [] );
 };
-cordova.addConstructor(OneTapSMSRetriever.install);
+
+/* Remove listner */
+exports.unregisterSMSListner = function(successCallback, failureCallback) {
+	exec(successCallback, failureCallback, 'OneTapSMSRetriever', 'UNREGISTER_OTP_SMS_LISTNER', [] );
+};
