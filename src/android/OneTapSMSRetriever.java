4d82f50ef49b155c7450d36f91325b53b833b02c
Eslam Sabry <eslam.sabry.a.t@gmail.com>
2022-03-10 09:37:23 +0300

    fix missing import

--- src/android/OneTapSMSRetriever.java
+++ src/android/OneTapSMSRetriever.java
@@ -9,6 +9,7 @@ import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import androidx.annotation.Nullable;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
