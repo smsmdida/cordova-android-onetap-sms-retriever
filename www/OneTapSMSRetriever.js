0e0a32b890c4cc455b78d8176a8edeba9aeed1c6
Eslam Sabry <eslam.sabry.a.t@gmail.com>
2022-03-10 10:43:00 +0300

    fix cordova.exec

--- www/OneTapSMSRetriever.js
+++ www/OneTapSMSRetriever.js
@@ -5,12 +5,12 @@ function OneTapSMSRetriever() {
 /* Start the SMS Retriever API and listen for incoming SMS and callback */
 
 OneTapSMSRetriever.prototype.registerSMSListner = function(successCallback, failureCallback) {
-	exec(successCallback, failureCallback, 'OneTapSMSRetriever', 'REGISTER_OTP_SMS_LISTNER', [] );
+	cordova.exec(successCallback, failureCallback, 'OneTapSMSRetriever', 'REGISTER_OTP_SMS_LISTNER', [] );
 };
 /* Remove listner */
 
 OneTapSMSRetriever.prototype.unregisterSMSListner = function(successCallback, failureCallback) {
-	exec(successCallback, failureCallback, 'OneTapSMSRetriever', 'UNREGISTER_OTP_SMS_LISTNER', [] );
+	cordova.exec(successCallback, failureCallback, 'OneTapSMSRetriever', 'UNREGISTER_OTP_SMS_LISTNER', [] );
 };
 
 OneTapSMSRetriever.install = function () {
